737bbc69e04fb2a4b516fc66230085f18d59e5a8
Omar Ham <omar.hp90@gmail.com>
2016-12-22 00:09:26 -0600

    Add solution for build heap problem

--- PriorityQueuesAndDisjointSets/BuildHeap.java
+++ PriorityQueuesAndDisjointSets/BuildHeap.java
@@ -19,36 +19,66 @@ public class BuildHeap {
         int n = in.nextInt();
         data = new int[n];
         for (int i = 0; i < n; ++i) {
-          data[i] = in.nextInt();
+            data[i] = in.nextInt();
         }
     }
 
     private void writeResponse() {
         out.println(swaps.size());
         for (Swap swap : swaps) {
-          out.println(swap.index1 + " " + swap.index2);
+            out.println(swap.index1 + " " + swap.index2);
         }
     }
 
     private void generateSwaps() {
-      swaps = new ArrayList<Swap>();
-      // The following naive implementation just sorts 
-      // the given sequence using selection sort algorithm
-      // and saves the resulting sequence of swaps.
-      // This turns the given array into a heap, 
-      // but in the worst case gives a quadratic number of swaps.
-      //
-      // TODO: replace by a more efficient implementation
-      for (int i = 0; i < data.length; ++i) {
-        for (int j = i + 1; j < data.length; ++j) {
-          if (data[i] > data[j]) {
-            swaps.add(new Swap(i, j));
-            int tmp = data[i];
-            data[i] = data[j];
-            data[j] = tmp;
-          }
+        swaps = new ArrayList<Swap>();
+        // The following naive implementation just sorts
+        // the given sequence using selection sort algorithm
+        // and saves the resulting sequence of swaps.
+        // This turns the given array into a heap,
+        // but in the worst case gives a quadratic number of swaps.
+        //
+        // TODO: replace by a more efficient implementation
+
+        int m=data.length/2;
+
+        for(int i=m; i>=0; i--){
+            siftDown(i);
         }
-      }
+    }
+
+    private void siftDown(int i){
+        int maxIndex=i;
+        int l=leftChild(i);
+        if(l<data.length && data[l]<data[maxIndex])
+            maxIndex=l;
+        int r=rightChild(i);
+        if(r<data.length && data[r]<data[maxIndex])
+            maxIndex=r;
+        if(i!=maxIndex){
+            swap(i, maxIndex);
+            swaps.add(new Swap(i, maxIndex));
+            siftDown(maxIndex);
+        }
+    }
+
+    private void swap(int i, int j){
+        int tmp=data[i];
+        data[i]=data[j];
+        data[j]=tmp;
+
+    }
+
+    private int parent(int i){
+        return (int)(i/2.0-0.5);
+    }
+
+    private int leftChild(int i){
+        return 2*i+1;
+    }
+
+    private int rightChild(int i){
+        return 2*i+2;
     }
 
     public void solve() throws IOException {
