ec7dc3fedc974227d294e8199e58355a22d5283e
Omar Ham <omar.hp90@gmail.com>
2016-12-30 08:44:54 -0600

    Add hash substring problem solution

--- HashTables/HashSubstring.java
+++ HashTables/HashSubstring.java
@@ -1,8 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class HashSubstring {
 
@@ -29,27 +26,53 @@ public class HashSubstring {
         }
     }
 
+    //RabinKarp algorithm
     private static List<Integer> getOccurrences(Data input) {
-        String s = input.pattern, t = input.text;
-        int m = s.length(), n = t.length();
+
+        int prime = 1000000007;
+        int x= new Random().nextInt(prime-1)+1;
         List<Integer> occurrences = new ArrayList<Integer>();
-        for (int i = 0; i + m <= n; ++i) {
-	    boolean equal = true;
-	    for (int j = 0; j < m; ++j) {
-		if (s.charAt(j) != t.charAt(i + j)) {
-		     equal = false;
- 		    break;
-		}
-	    }
-            if (equal)
-                occurrences.add(i);
-	}
+        int pHash=polyHash(input.pattern,prime,x);
+        int[] h=precomputeHashes(input.text, input.pattern.length(), prime, x);
+        for(int i=0; i<=input.text.length()-input.pattern.length(); i++){
+            if(pHash==h[i]){
+                if(input.text.substring(i,i+input.pattern.length()).equals(input.pattern))
+                    occurrences.add(i);
+            }
+        }
         return occurrences;
+    }
+
+    private static int polyHash(String str, int p, int x) {
+        int hash = 0;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            hash = (int)(((long)hash * x + str.charAt(i)) % p);
+        }
+        return hash;
+    }
+
+    private static int[] precomputeHashes(String text, int patternLength, int prime, int multiplier) {
+        int[] h = new int[text.length() - patternLength + 1];
+        String sub = text.substring(text.length() - patternLength, text.length());
+        h[text.length() - patternLength] = polyHash(sub, prime, multiplier);
+        int y = 1;
+        for (int i = 1; i <= patternLength; i++)
+            y = (int)(((long)y * multiplier) % prime);
+        for (int i = text.length() - patternLength - 1; i >= 0; i--) {
+//            long a = multiplier *(long)h[i+1];
+//            long b=text.charAt(i);
+//            long c=text.charAt(i+patternLength);
+//            long d=-y*c;
+//            h[i]=(int)((((a+b+d)%prime)+prime)%prime);
+            h[i] = (int) (((multiplier * (long) h[i + 1] + (long) text.charAt(i) - (long) y * text.charAt(i + patternLength)) % prime+prime)%prime);
+        }
+        return h;
     }
 
     static class Data {
         String pattern;
         String text;
+
         public Data(String pattern, String text) {
             this.pattern = pattern;
             this.text = text;
